83b9e99d3e37486092b2c14ea9e2a707213f14fa
gaoluping <lcc6048@gmail.com>
2025-02-10 15:37:56 +0800

    day2 白屏检测

--- src/monitor/index.js
+++ src/monitor/index.js
@@ -1,3 +1,7 @@
+const { injectBlankScreen } = require('./lib/blankScreen');
 const { injectJsError } = require('./lib/jsError');
+const { injectXHR } = require('./lib/xhrError');
 
 injectJsError();
+injectXHR();
+injectBlankScreen();
---
+++ src/monitor/lib/blankScreen.js
@@ -0,0 +1,61 @@
+import getSelector from '../utils/getSelector';
+import onload from '../utils/onload';
+import tracker from '../utils/tracker';
+
+export function injectBlankScreen() {
+  const wrapperElements = ['html', 'body', '#container', '.content'];
+  let emptyPoints = 0;
+
+  function getSelector(element) {
+    if (element.id) {
+      return `#${element.id}`;
+    }
+    if (element.className) {
+      // a b c => .a.b.c
+      const className = element.className
+        .split(' ')
+        .filter((item) => !!item)
+        .join('.');
+      return `.${className}`;
+    }
+    return element.nodeName.toLowerCase();
+  }
+
+  function isWrapper(element) {
+    const selector = getSelector(element);
+    if (wrapperElements.indexOf(selector) !== -1) {
+      emptyPoints++;
+    }
+  }
+
+  onload(function () {
+    for (let i = 1; i <= 9; i++) {
+      let xElements = document.elementsFromPoint(
+        (window.innerWidth * i) / 10,
+        window.innerHeight / 2
+      );
+      let yElements = document.elementsFromPoint(
+        window.innerWidth / 2,
+        (window.innerHeight * i) / 10
+      );
+      isWrapper(xElements[0]);
+      isWrapper(yElements[0]);
+    }
+    console.log('emptyPoints', emptyPoints);
+
+    if (emptyPoints > 16) {
+      const centerElement = document.elementFromPoint(
+        window.innerWidth / 2,
+        window.innerHeight / 2
+      );
+      tracker.send({
+        kind: 'stability',
+        type: 'blank',
+        emptyPoints,
+        screen: window.screen.width + 'X' + window.screen.height,
+        viewPoint: window.innerWidth + 'X' + window.innerHeight,
+        selector: getSelector(centerElement),
+      });
+    }
+  });
+}
